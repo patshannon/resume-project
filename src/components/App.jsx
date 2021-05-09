eb0e0ab596e515779c5e195b3f674ccd5ad331b5
Patrick Shannon <peshannon104@gmail.com>
2021-05-09 16:05:21 -0300

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { MemoryRouter, Switch, Route, BrowserRouter as Router, Redirect, Link, NavLink } from 'react-router-dom';
+import {CSSTransition} from "react-transition-group";
+
+import Home from "./Home/Home"
+import About from "./About/About"
+import Contact from "./Contact/Contact"
+import { logDOM } from "@testing-library/dom";
+
+
+const routes = [
+  { path: '/', name: 'Home', Component: Home },
+  { path: '/about', name: 'About', Component: About },
+  { path: '/contact', name: 'Contact', Component: Contact },
+]
+
+
+function App(){
+  return(
+
+    <Router>
+      <>
+      <div className="padding-16">
+            <nav className="justify-center">
+            {routes.map(route =>(
+              <NavLink
+                key={route.path}
+                to={route.path}
+                activeClassName="active"
+                className="nav-margin nav-padding nav-btn"
+                exact
+                >
+                  {route.name}
+              </NavLink>
+            ))}
+       
+          </nav>
+        </div>
+
+        
+          
+          <div>
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <div className="page">
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </div>
+      </>
+    </Router>
+)
+  }
+
+export default App;
