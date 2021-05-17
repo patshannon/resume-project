c5a1d8ffbbfc6491b460971af34bc8a7f4831d1b
Patrick Shannon <peshannon104@gmail.com>
2021-05-17 14:10:03 -0300

    update contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,21 +1,72 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from "../Footer"
 import emailjs from 'emailjs-com'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Redirect } from 'react-router';
 
 
 
 export default function Contact() {
 
-    function sendEmail(e) {
-      e.preventDefault();
-  
-      emailjs.sendForm('default_service', 'template_vaw0ggo', e.target,"user_AoEJ0fNkPUEePesYyHNIV")
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
+    const [user, setText] = useState({
+        user_name: "",
+        user_email: "",
+        message: ""
+    });
+
+    
+
+    function handleChange(e){
+        const {name, value} = e.target;
+        setText((prevValue)=>{
+            return{
+                ...prevValue,
+                [name]: value
+            };
+
         });
+        console.log(user)
+    }
+
+    const [isSent, setSent] = useState(false);    
+
+    function sendEmail(e) {
+
+            if (user.user_name !== "" && user.message !== "" && user.user_email!==""){
+
+                e.preventDefault();
+                emailjs.sendForm('default_service', 'template_vaw0ggo', e.target,"user_AoEJ0fNkPUEePesYyHNIV") //insert user_AoE etc.
+                .then((result) => {
+                    console.log(result.text);
+                    toast.info('Message Sent!', {
+                        position: "bottom-center",
+                        autoClose: 2000,
+                        hideProgressBar: true,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        });
+                    setSent(true);
+                    setTimeout(()=> window.location.reload(),2000)
+
+                }, (error) => {
+                e.preventDefault();
+                console.log(error.text);
+                alert("There was an error please try again!");
+                
+                 }); 
+                
+            }else{
+                e.preventDefault();
+                alert("Please Enter All Your Info!");
+                
+            }
+            
+        
+      
     }
   
     return (
@@ -23,13 +74,24 @@ export default function Contact() {
             <div className="col-12 contentBox contactBox">
                 <h1 className="center padding-16 icon-grey">Get in Touch!</h1>
                 <form className="contact-form" onSubmit={sendEmail}>
-                    <input className="inputBox"type="text" name="user_name" id="uname" placeholder="Name"/>
-                    <input className="inputBox"type="email" name="user_email" id="uemail" placeholder="Email" /> 
-                    <textarea name="message" id="messageBox" className="inputBox" placeholder="Type Message Here..."/>
-                    <input id="send" className="btn btn-primary margin-bottom" type="submit" value="Send" />
+                    <input onChange = {handleChange} className="inputBox" type="text" name="user_name" id="uname" placeholder="Name"/>
+                    <input onChange = {handleChange} className="inputBox" type="email" name="user_email" id="uemail" placeholder="Email" /> 
+                    <textarea onChange = {handleChange} name="message" id="messageBox" className="inputBox" placeholder="Type Message Here..."/>
+                    <input id="send" className={`btn btn-primary margin-bottom ${isSent ? "greenCheck" : ""}`} value={`${isSent ? "Sent!" : "Send"}`} type="submit" />
                 </form>
             </div>
-            <Footer />
+            <ToastContainer
+                position="bottom-center"
+                autoClose={2000}
+                hideProgressBar={true}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                />
+            <Footer class="contactFooter"/>
         </div>
     );
   }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Footer() {
+function Footer(props) {
   const year = new Date().getFullYear();
   return (
-    <footer className="info-box center margin-top">
+    <footer className={`info-box center margin-top ${props.class}`}>
       <p>Find me on social media.</p>
       <a className="social-link" href="https://www.facebook.com/patrick.shannon.94"><i className="fa fa-facebook-official opacity"></i></a>
       <a className="social-link" href="https://www.linkedin.com/in/patrick-e-shannon"><i className="fa fa-linkedin opacity"></i></a>
